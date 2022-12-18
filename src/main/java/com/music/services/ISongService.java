9f726724c3e72f91907907f4bb1f6c050b4aca59
Akashkanaparthi99 <88332407+Akashkanaparthi99@users.noreply.github.com>
2022-12-18 15:34:31 +0530

    Update ISongService.java

--- src/main/java/com/music/services/ISongService.java
+++ src/main/java/com/music/services/ISongService.java
@@ -1,5 +1,11 @@
 package com.music.services;
+import com.music.model.Song;
 
+import java.util.List;
 public interface ISongService {
-
+   public void addSong(Song song);
+    public void updateSong(Song song);
+    public void deleteSong(int songId);
+    public Song getById(int songId);
+    public List<Song> getAll();
 }
